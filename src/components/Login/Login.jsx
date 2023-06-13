defca1530df557679df2bce290b041987b8fd826
romanfaria <roman.faria@gmail.com>
2023-06-13 15:29:58 -0400

    added framework components and changed AllProducts to AllPosts. Register and Login functionality now working

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,59 @@
+import { loginUser } from "../api-adapters";
+import { useNavigate, Link } from "react-router-dom";
+import React, { useState } from "react";
+
+const Login = (props) => {
+  const setIsLoggedIn = props.setIsLoggedIn;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const result = await loginUser(username, password);
+      console.log(result);
+
+      localStorage.setItem("token", result.token);
+      setIsLoggedIn(true);
+
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </label>
+
+        <button type="submit">Login</button>
+      </form>
+      <Link to="/register">Don't have an account? Click Here!</Link>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/api-adapters/index.js
@@ -0,0 +1,49 @@
+const COHORT_NAME = "2304-FTB-ET-WEB-FT";
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
+
+export const registerUser = async (username, password) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: username,
+          password: password,
+        },
+      }),
+    });
+
+    const result = await response.json();
+
+    return result.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const loginUser = async (username, password) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: username,
+          password: password,
+        },
+      }),
+    });
+
+    const result = await response.json();
+    console.log(result);
+
+    return result.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
